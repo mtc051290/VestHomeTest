8a8ade4073b263c3c42d5c780b6faeecff30e474
Miguel Torres <91702275+a01168868@users.noreply.github.com>
2022-01-10 01:59:30 -0600

    Adding Heroku Credentials

--- utils/database.py
+++ utils/database.py
@@ -3,10 +3,9 @@ sys.path.append("..")
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-from utils.exceptions import database_exception
 
-
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:Miguel.01@127.0.0.1:3306/vest_mtc"
+# Connect to Heroku MySQL 
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://bccf25ad9ef0b8:19d1cdea@us-cdbr-east-05.cleardb.net:3306/heroku_376a42443d2d0ac"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
