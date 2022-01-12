131be42fc91a394013f6e0436d58527bee83c44c
mtc051290 <mtc590@hotmail.com>
2022-01-12 17:41:10 -0600

    Settings

--- routers/auth.py
+++ routers/auth.py
@@ -58,13 +58,6 @@ class LoginForm:
 
 
 
-
-
-
-
-
-
-
 def get_password_hash(password):
     return bcrypt_context.hash(password)
 
@@ -116,9 +109,6 @@ async def get_current_user(request: Request):
 
 
 
-
-
-# Not used in fullStack
 @router.post("/create/user", response_model=users.CreatedUser, status_code=status.HTTP_201_CREATED)
 async def create_new_user(create_user: users.CreateUser,db: Session=Depends(get_db)):
     try:
@@ -139,8 +129,6 @@ async def create_new_user(create_user: users.CreateUser,db: Session=Depends(get_
         raise exceptions.bad_user_create_request_exception()
 
 
-
-
 @router.post("/token")
 async def login_for_access_token(response: Response,
                                 form_data: OAuth2PasswordRequestForm=Depends(),
@@ -152,98 +140,6 @@ async def login_for_access_token(response: Response,
     token=create_access_token(user.username, user.id, expires_delta=token_expires)
     response.set_cookie(key="access_token", value=token, httponly=True)
     return True
-
-
-
-
-
-@router.get("/", response_class=HTMLResponse)
-async def authentication_page(request: Request):
-    return templates.TemplateResponse("login.html", {"request": request})
-
-
-
-@router.post("/", response_class=HTMLResponse)
-async def login(request: Request, db: Session=Depends(get_db)):
-    try:
-        form = LoginForm(request)
-        await form.create_oauth_form()
-        response=RedirectResponse(url="/todos", status_code=status.HTTP_302_FOUND)
-
-        validate_user_cookie=await login_for_access_token(
-            response=response, form_data=form, db=db
-        )
-
-        if not validate_user_cookie:
-            msg = "Incorrect Username or password"
-            return templates.TemplateResponse("login.html",{"request":request, "msg":msg})
-        return response
-    except HTTPException:
-        msg="Unknown Error"
-        return templates.TemplateResponse("login.html",{"request":request, "msg":msg})
-
-
-
-
-
-
-
-
-@router.get("/logout")
-async def logout(request: Request):
-    msg="Logout Successful"
-    response=templates.TemplateResponse("login.html",{"request":request, "msg":msg})
-    response.delete_cookie(key="access_token")
-    return response
-
-
-
-
-
-
-
-
-@router.get("/register", response_class=HTMLResponse)
-async def register(request: Request):
-    return templates.TemplateResponse("register.html", {"request": request})
-
-
-
-
-
-
-
-
-@router.post("/register", response_class=HTMLResponse)
-async def register_user(request: Request, email: str=Form(...), 
-                        username: str=Form(...), firstname:str=Form(...),
-                        lastname:str=Form(...), password:str=Form(...),
-                        password2:str=Form(...),
-                        db: Session=Depends(get_db)):
-    validation1=db.query(models.Users).filter(models.Users.username==username).first()
-    validation2=db.query(models.Users).filter(models.Users.email==email).first()
-
-    if password!=password2 or validation1 is not None or validation2 is not None:
-        msg="Invalid registration request"
-        return templates.TemplateResponse("register.html", {"request":request,"msg":msg})
-    
-    user_model=models.Users()
-    user_model.username=username
-    user_model.email=email
-    user_model.first_name=firstname
-    user_model.last_name=lastname
-
-    hash_password=get_password_hash(password)
-    user_model.hashed_password=hash_password
-    user_model.is_active=True
-
-    db.add(user_model)
-    db.commit()
-
-    msg="User successfully created"
-    return templates.TemplateResponse("login.html", {"request":request,"msg":msg})
-
-
 
 
 
